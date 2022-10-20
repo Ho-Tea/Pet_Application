880236193852be6b8610b2c7e868a38cbe6a623a
yoonjuho <dbswn990@naver.com>
2022-10-20 22:13:01 +0900

    feat: init

---
+++ app/src/main/java/com/example/pet_application/Login.kt
@@ -0,0 +1,6 @@
+package com.example.pet_application
+
+data class Login(
+    val code: String,
+    val msg: String
+)   //output으로 서버측에서부터 받아올 정보들)
---
+++ app/src/main/java/com/example/pet_application/service/LoginService.kt
@@ -0,0 +1,8 @@
+package com.example.pet_application.service
+
+import android.telecom.Call
+import com.example.pet_application.Login
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
