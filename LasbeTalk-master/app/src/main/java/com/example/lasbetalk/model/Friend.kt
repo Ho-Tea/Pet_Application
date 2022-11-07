cf0ae57949ca5e0369a74297c023dfc09ff1cebf
yoonjuho <dbswn990@naver.com>
2022-11-08 05:53:02 +0900

    feat: pet Application

--- LasbeTalk-master/app/src/main/java/com/example/lasbetalk/model/Friend.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lasbetalk.model
-
-data class Friend(
-    val email : String? = null,
-    val name : String? = null,
-    val profileImageUrl : String? = null,
-    val uid : String? = null)
--- app/src/main/java/com/example/pet_application/Login.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pet_application
-
-data class Login(
-    val code: String,
-    val msg: String
-)   //output으로 서버측에서부터 받아올 정보들)
