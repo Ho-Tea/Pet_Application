025aa0b063618859fc35ccc1fdd817dd168222dd
yoonjuho <dbswn990@naver.com>
2022-11-28 18:42:57 +0900

    feat: data class 추가

---
+++ HappyDog/app/src/main/java/com/example/happydog/model/Profile.kt
@@ -0,0 +1,14 @@
+package com.example.happydog.model
+
+data class Profile(
+    val email : String? = null,
+    val name : String? = null,
+    val profileImageUrl : String? = null,
+    val uid : String? = null,
+    val age : String? = null,
+    val sex : String? = null,
+    val kg : String? = null,
+    val type : String? = null,
+    val location : String? = null,
+    val tendency : String? = null
+    )
